85dbff33143f0389495153b84a80b1b066691d44
Nm836 <127262410+Nm836@users.noreply.github.com>
2024-09-26 19:50:48 +1000

    Update ConnectionCheck.php

--- ConnectionCheck.php
+++ ConnectionCheck.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:qrtrack-server.database.windows.net,1433; Database = qrtrack_sample", "Nm836", "Capstone@123");
+    $conn = new PDO("sqlsrv:server = tcp:qrtrack-server.database.windows.net,1433; Database = qrtrack_sample", "Nm836", "Capstone@123","Encrypt" => true);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
