bbe2f9927ca38cd13c33bcbefd717fb6f883bdd0
Nm836 <127262410+Nm836@users.noreply.github.com>
2024-09-26 21:50:08 +1000

    Update ConnectionCheck.php

--- ConnectionCheck.php
+++ ConnectionCheck.php
@@ -1,13 +1,31 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:qrtrack-server.database.windows.net,1433; Database = qrtrack_sample", "Nm836", "Capstone@123","Encrypt" => true);
+    $conn = new PDO("sqlsrv:server = tcp:qrtrack-server.database.windows.net,1433; Database = qrtrack_sample", "Nm836", "Capstone@123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Nm836", "pwd" => "Capstone@123", "Database" => "qrtrack_sample", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:qrtrack-server.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
+
+//<?php
+// PHP Data Objects(PDO) Sample Code:
+//try {
+  //  $conn = new PDO("sqlsrv:server = tcp:qrtrack-server.database.windows.net,1433; Database = qrtrack_sample", "Nm836", "Capstone@123","Encrypt" => true);
+    //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//}
+//catch (PDOException $e) {
+  //  print("Error connecting to SQL Server.");
+    //die(print_r($e));
+//}
 
 // SQL Server Extension Sample Code:
 //$connectionInfo = array("UID" => "qrtrack_server", "pwd" => "{your_password_here}", "Database" => "QRtrack", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
