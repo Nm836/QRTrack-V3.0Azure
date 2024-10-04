1e064c259613d9ccf49f7a89fe16f5fcec201e91
sobanbadonia07 <166586472+sobanbadonia07@users.noreply.github.com>
2024-10-04 17:26:04 +1000

    Conn check4

--- 6_StaffPage.php
+++ 6_StaffPage.php
@@ -40,9 +40,11 @@ session_start();
             
         </form>
 <!--QR Code File path-->
+<button onclick="window.location.href='/QR Generator main file.html'">Generate QR Code</button>
+<!--
 <form action="/QR Generator main file.html" method="POST" style="display:flex">
     <input type='submit' name='QRCodeGenerator' value='Generate QR Code'>
-</form>
+</form> -->
         <!-- CSV Download Button -->
 <form action="download_csv.php" method="POST" style="display:flex">
    
