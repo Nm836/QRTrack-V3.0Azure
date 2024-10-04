2f3607301e6dfb846faf307facd6497ea416950c
sobanbadonia07 <166586472+sobanbadonia07@users.noreply.github.com>
2024-10-04 18:26:17 +1000

    conn check

--- 6_StaffPage.php
+++ 6_StaffPage.php
@@ -40,7 +40,7 @@ session_start();
             
         </form>
 <!--QR Code File path-->
-<button onclick="window.location.href='/Qr_Generator/templates/QR_Generator_Info.html'" style="display:flex ">Generate QR Code</button>
+<button onclick="window.location.href='/QR_Generator_Info.html'" style="display:flex ">Generate QR Code</button>
 <!--QR Code File path-->
 
 
