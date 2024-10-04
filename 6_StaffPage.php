167451c7fbddd94a3f62c78d2c7134075e0bdf20
sobanbadonia07 <166586472+sobanbadonia07@users.noreply.github.com>
2024-10-04 18:17:05 +1000

    Check filw runing in template folder

--- 6_StaffPage.php
+++ 6_StaffPage.php
@@ -40,7 +40,7 @@ session_start();
             
         </form>
 <!--QR Code File path-->
-<button onclick="window.location.href='QR_Generator_Info.html'" style="display:flex ">Generate QR Code</button>
+<button onclick="window.location.href='/templates/QR_Generator_Info.html'" style="display:flex ">Generate QR Code</button>
 <!--QR Code File path-->
 
 
