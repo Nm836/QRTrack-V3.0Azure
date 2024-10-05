40c57210db2b3f14d09cb0bac7796ce32efbd5aa
Nm836 <127262410+Nm836@users.noreply.github.com>
2024-10-05 12:01:32 +1000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 pip install -r /home/site/wwwroot/requirements.txt || { echo 'Failed to install Python dependencies'; exit 1; }
 
 # Start the Python Flask app in the background
-nohup python3 /home/site/wwwroot/app.py &
+nohup python3.9 /home/site/wwwroot/app.py &
 
 # Start the PHP application using Apache
 apache2-foreground
