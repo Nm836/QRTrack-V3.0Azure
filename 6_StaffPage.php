f617281541dcd21df55b6a49cd8bcaccd3a06d53
sobanbadonia07 <166586472+sobanbadonia07@users.noreply.github.com>
2024-10-09 17:30:24 +1100

    all record check

--- 6_StaffPage.php
+++ 6_StaffPage.php
@@ -43,6 +43,8 @@ session_start();
             
         </form>
 
+        <button onclick="window.location.href='/All record.php'" style="display:flex ">All record</button>        
+
 <!--QR Code File path
 <button onclick="window.location.href='/QR_Generator_Info.php'" style="display:flex ">Generate QR Code</button>
 -->
