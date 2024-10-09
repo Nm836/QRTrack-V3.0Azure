d49f125d4bbae614f52b8870838a8267d3fe18fb
sobanbadonia07 <166586472+sobanbadonia07@users.noreply.github.com>
2024-10-09 22:44:57 +1100

    logout

--- 6_StaffPage.php
+++ 6_StaffPage.php
@@ -19,7 +19,7 @@ session_start();
         </h1>
     </header>
             <form action='index.php' method='POST' >
-                <input type='submit' name='logout' value='Log Out' class="buttons">
+                <input type='submit' name='logout' value='Log Out' class='logout-button'>
             </form>
     <main>
         <?php
